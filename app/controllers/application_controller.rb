4bbe036833b88fbd2b2d13b1d0fd40f2be6a08a5
Yina Zhu <yinagit@gmail.com>
2018-07-05 05:34:34 +0000

    added models and basic migrations

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "cinephile
+    _secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do #helper methods
+    def logged_in?
+      !!current_user #returns value true or false
+    end
+
+    def current_user #should return previous user or look for current user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
+
+
+end
---
+++ db/migrate/20180705052053_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.integer :rating
+      t.string :review
+
+      t.timestamps null: false
+    end
+  end
+end
