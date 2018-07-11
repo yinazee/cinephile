7546cdf27c8efce508865d247d283dc5409df618
Yina Zhu <yinagit@gmail.com>
2018-07-11 02:34:38 +0000

    able to scrape and create director instances

--- app/models/dir_scraper.rb
+++ app/models/dir_scraper.rb
@@ -6,8 +6,20 @@ class DirScraper < ActiveRecord::Base
   def self.scrape_url
     #scrapes 300 directors so it could display in a drop down list
     doc = Nokogiri::HTML(open(@@url))
-binding.pry
-    dirscrape = doc.search("div.list").text.gsub("\n", " ")
+    list = doc.search("tbody td.td16a div.list").text.split("\n")
+    list.shift
+    list
+    list.each do |filmmaker|
+      filmmaker = Director.create
+    end
+    # Director objects are being created but the names are nil
+    binding.pry
+    # a list of filmmakers
+    # filmmakers = list.slice(0, list.length)
+    # the first one is nil and needs to be removed
+    # will work on removing integers using g.sub
+    # and then sorting them abc order ascending
+
 
 
   end
