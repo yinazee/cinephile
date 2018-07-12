92573b1d19add38a2ed253a98fd0667ca282ddb8
Yina Zhu <yinagit@gmail.com>
2018-07-12 05:37:23 +0000

    quick save

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -1,2 +1,9 @@
 class DirectorController < ApplicationController
+
+  get '/directors/show'
+  @directors = Director.all
+  # if Director.count < 300
+  #   DirScraper.scrape_url
+  # end
+  erb :'/directors/show'
 end
